7e77270d01932ca8597280c3251bdfe50ce4f713
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2022-11-11 10:53:00 +0200

    Update Presentation.java
    Зміни пов"язані з некоректним виводом результату у консоль, а саме в начальному варіанті виводились коректно тільки окремі букви, а слова змішувалися.

--- src/map/lessonHome4/Presentation.java
+++ src/map/lessonHome4/Presentation.java
@@ -5,17 +5,19 @@ import java.util.Map;
 
 public class Presentation {
 
-    public static void inputWords (String input) {
-        input = input.toLowerCase();
+    public static void inputWords(String input) {
         ArtASCII art = new ArtASCII();
-        char[] word = input.toCharArray();
-        Map<Character, String[]> map = art.getMap();
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < word.length; j++) {
-                String[] wordArt = map.get(word[j]);
-                System.out.println(wordArt[i] + " ");
+        input = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            String word = "" + input.charAt(i);
+            Map<Character, String[]> map = art.getMap();
+            for (int k = 0; k < 6; k++) {
+                for (int j = 0; j < word.length(); j++) {
+                    System.out.println("" + map.get(word.charAt(j))[k]);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
+
